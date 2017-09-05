21b5998422970d11ffe01ee3203f0183d6589d61
Isabelle <isabelle@mac-isabelle.speedport_w_921v_1_43_000>
2017-09-05 11:46:46 +0200

    First commit

---
+++ Emoji Dictionary/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Emoji Dictionary
+//
+//  Created by Isabelle on 05.09.17.
+//  Copyright © 2017 Isi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
